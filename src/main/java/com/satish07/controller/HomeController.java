f2892f72828737ec10650cb267ef3850d0bad3da
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-04 00:19:35 +0530

    @RequestMapping Annotation
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/satish07/controller/HomeController.java
+++ src/main/java/com/satish07/controller/HomeController.java
@@ -7,12 +7,14 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/first")
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping(path = "/home", method = RequestMethod.GET)
 	public String home(Model model) {
 		System.out.println("this is home url");
 		model.addAttribute("name","Satish Kumar Gupta");
